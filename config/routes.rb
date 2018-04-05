58d7cb47cf647b1a5056b8be49b606f7ba805104
Huynh Dac Hoang <hoang.itsolutions@gmail.com>
2018-04-05 14:00:31 +0700

    Finish Chapter 3
    Add a Static Pages controller
    
    Chapter 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
+<<<<<<< 810f04884ac6568faf42e65921b20a7e69012cc7
 <<<<<<< HEAD
 #   root 'static_pages#home'
   get  'static_pages/home'
   get  'static_pages/help'
   get  'static_pages/about'
 =======
+=======
+>>>>>>> Finish Chapter 3
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
+<<<<<<< 810f04884ac6568faf42e65921b20a7e69012cc7
 >>>>>>> 09d7bfa... Finish Chapter 4
+=======
+>>>>>>> Finish Chapter 3
 end
