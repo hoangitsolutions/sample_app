1d5a9cd2a83fbd9ab3329cc6eaca22b21a17e0e3
Huynh Dac Hoang <hoang.itsolutions@gmail.com>
2018-04-05 14:00:31 +0700

    Finish Chapter 3
    Add a Static Pages controller
    
    Chapter 3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
   def home; end
+<<<<<<< 810f04884ac6568faf42e65921b20a7e69012cc7
 
   def help; end
+=======
+  def help; end
+  def about; end
+>>>>>>> Finish Chapter 3
 end
