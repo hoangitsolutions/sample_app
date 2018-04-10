69452a126fb250492998fc91c319f4b5a8bacde8
Huynh Dac Hoang <hoang.itsolutions@gmail.com>
2018-04-10 16:45:03 +0700

    chapter 5

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,9 @@
 class StaticPagesController < ApplicationController
   def home; end
-<<<<<<< 810f04884ac6568faf42e65921b20a7e69012cc7
 
   def help; end
-=======
-  def help; end
+
   def about; end
->>>>>>> Finish Chapter 3
+
+  def contact; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< 810f04884ac6568faf42e65921b20a7e69012cc7
-<<<<<<< HEAD
-#   root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
-=======
-=======
->>>>>>> Finish Chapter 3
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
-<<<<<<< 810f04884ac6568faf42e65921b20a7e69012cc7
->>>>>>> 09d7bfa... Finish Chapter 4
-=======
->>>>>>> Finish Chapter 3
+  get "users/new"
+  root "static_pages#home"
+  get "/help", to: "static_pages#help"
+  get "/contact", to: "static_pages#contact"
+  get "/about", to: "static_pages#about"
+  get "/signup", to: "users#new"
 end
